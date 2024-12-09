f2e212782d3563c5dc6be0044141f15b743159e6
AryaK19 <aryakadam348@gmail.com>
2024-12-09 23:45:52 +0530

    added componented and adgusted come components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './layout/Navbar';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Footer from './layout/Footer';
+import Footer from './components/Footer';
 import Dashboard from './pages/Dashboard';
 import GrammarAssessment from './pages/GrammarAssessment';
 
